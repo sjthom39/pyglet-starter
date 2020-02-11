72dfb265d4d22cd33cad5c7fc7df2db104055917
Stephen Thomas <sjthom39>
2020-02-11 10:42:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ window = pyglet.window.Window() # create the window
 # label = pyglet.text.Label('Hello, world', x = 200, y = 200)
 
 # Create a sprite
-ball_image = pyglet.image.load('assets/hero/Old hero.png')
-ball = pyglet.sprite.Sprite(ball_image, x=50, y=50)
-
+img = pyglet.image.load('samuri')
 # Start the event loop
 @window.event
 def on_draw():
